db0cccd06676437c16ff9416c02d087951c23c8e
prozaso <p.rozas.omar@gmail.com>
2021-09-22 04:55:58 -0300

    buscarRdy

--- app/views.py
+++ app/views.py
@@ -12,10 +12,27 @@ def home(request):
 
 
 def servicios(request):
+    
+    if 'buscar' in request.POST:
+        if request.method == 'POST':
+            id = request.POST.get('cboserv')
+            #print(id)
+    if 'eliminar' in request.POST:
+        if request.method == 'POST':
+            id = request.POST.get('cboserv')
+            salida = eliminar_servicio(id)
+            if salida == 1:
+                data['eliminar'] = 'Servicio eliminado correctamente'
+            else:
+                data['eliminar'] = 'Hubo un error al eliminar el servicio'
+    
     data = {
-        'servicios': lista_servicios()
+        'servicios' : lista_servicios(),
+        'servicio'  : servicio_buscar(id)
     }
-    print(servicios)
+
+    print(lista_servicios())
+
     return render(request, 'app/servicios.html', data)
 
 
@@ -32,6 +49,38 @@ def lista_servicios():
         lista.append(fila)
 
     return lista
+
+
+def servicio_buscar(servid):
+
+    django_cursor = connection.cursor()
+    cursor = django_cursor.connection.cursor()
+    out_cur = django_cursor.connection.cursor()
+
+    cursor.callproc("SP_BUSCAR_SERVICIO", [servid, out_cur])
+
+    lista = []
+    for fila in out_cur:
+        lista.append(fila)
+
+    return lista
+
+
+def eliminar_servicio(servid):
+
+    django_cursor = connection.cursor()
+    cursor = django_cursor.connection.cursor()
+    salida = cursor.var(cx_Oracle.NUMBER)
+    cursor.callproc('SP_ELIMINAR_SERVICIO', [servid, salida])
+
+    return salida.getvalue()
+
+
+
+
+
+
+
 
 
 def lista_regiones():
