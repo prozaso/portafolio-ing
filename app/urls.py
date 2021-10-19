e2a7db4864b0155e3a77c57514bfff1b120c95dc
prozaso <p.rozas.omar@gmail.com>
2021-10-19 00:32:19 -0300

    rubrosRDY

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import views, servicios, usuarios, capacitaciones
+from .views import views, servicios, usuarios, capacitaciones, rubros
 
 
 urlpatterns = [
     path('', views.home, name="home"),
     path('servicios', servicios.servicios, name="servicios"),
     path('registro_usuarios', usuarios.registro_usuarios, name='registro_usuarios'),
-    path('capacitaciones', capacitaciones.capacitaciones, name='capacitaciones')
+    path('capacitaciones', capacitaciones.capacitaciones, name='capacitaciones'),
+    path('rubros', rubros.rubros, name='rubros')
 
 ]
